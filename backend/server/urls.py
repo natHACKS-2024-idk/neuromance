2e9986f70bdce75f828c528960c764dd45575cd6
Khyl Nad <khylanad@gmail.com>
2024-11-15 16:30:49 -0700

    made neuromanceDB app the default url

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("", include("neuromanceDB.urls")),
     path('admin/', admin.site.urls),
-    path("neuromanceDB/", include("neuromanceDB.urls")),
 ]

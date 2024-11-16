674c22127e9f462e3924555cd6458b15d5442dc6
git-ashira <ahira1@ualberta.ca>
2024-11-15 21:19:09 -0700

    matchListUI

---
+++ frontend/neuromance/src/app/components/MatchList/MatchList.tsx
@@ -0,0 +1,11 @@
+import styles from "./MatchList.module.css";
+import MatchListItem from "../MatchListItem/MatchListItem";
+
+export default function MatchList() {
+
+    return (
+
+        <MatchListItem/>
+
+    );
+}

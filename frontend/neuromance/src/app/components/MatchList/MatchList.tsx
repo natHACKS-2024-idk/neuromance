109ca866db768a181a84f8c8cffe863bb3ec98e7
git-ashira <ahira1@ualberta.ca>
2024-11-16 16:25:04 -0700

    navBar

--- frontend/neuromance/src/app/components/MatchList/MatchList.tsx
+++ frontend/neuromance/src/app/components/MatchList/MatchList.tsx
@@ -1,11 +1,13 @@
 import styles from "./MatchList.module.css";
 import MatchListItem from "../MatchListItem/MatchListItem";
+import NavBar from "../NavBar/NavBar";
 
 export default function MatchList() {
 
     return (
-
+        <>
+        <NavBar/>
         <MatchListItem/>
-
+        </>
     );
 }

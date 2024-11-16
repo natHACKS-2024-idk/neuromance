2559487223f2c2f1cc97e8d77310ef358b53edf2
Khyl Nad <khylanad@gmail.com>
2024-11-15 18:21:09 -0700

    refactored page.tsx to handle multiple routes - used react-router-dom for routing - created AppRouter.tsx component to handle routing - configured next.config.ts to include AppRouter paths

--- frontend/neuromance/next.config.ts
+++ frontend/neuromance/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/", // All unmatched paths go to the root page
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- frontend/neuromance/src/app/page.tsx
+++ frontend/neuromance/src/app/page.tsx
@@ -1,51 +1,19 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import styles from "./page.module.css";
-import { MuseClient, zipSamples } from "muse-js";
-import { useState } from "react";
+
+// Dynamically import your component to disable SSR
+const AppRouter = dynamic(() => import("./components/AppRouter/AppRouter"), {
+  ssr: false,
+});
 
 export default function Home() {
-  const [isConnecting, setIsConnecting] = useState(false);
-  const [connected, setConnected] = useState(false);
-  const [client, setClient] = useState<MuseClient | null>(null);
-
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {!connected ? (
-          <button onClick={connect}>Connect</button>
-        ) : (
-          <button onClick={disconnect}>Disconnect</button>
-        )}
+        <AppRouter />
       </main>
     </div>
   );
-
-  async function connect() {
-    try {
-      setIsConnecting(true);
-
-      let client = new MuseClient();
-      await client.connect();
-      await client.start();
-
-      client.eegReadings.subscribe((reading) => {
-        console.log(reading);
-      });
-
-      setClient(client);
-      setConnected(true);
-    } catch (err) {
-      console.error("connect():", err);
-      setClient(null);
-      setConnected(false);
-    } finally {
-      setIsConnecting(false);
-    }
-  }
-
-  async function disconnect() {
-    setClient(null);
-    setConnected(false);
-  }
 }

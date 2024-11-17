11acc475b93c88bee02eea5315ff768274d519ad
git-ashira <ahira1@ualberta.ca>
2024-11-17 08:23:11 -0700

    editing the youtube link

--- frontend/neuromance/src/app/components/ReadMuse/ReadMuse.tsx
+++ frontend/neuromance/src/app/components/ReadMuse/ReadMuse.tsx
@@ -171,7 +171,7 @@ export default function ReadMuse() {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/U8VBKOoe-iY?si=HPXYCvhOMZbs6QUn"
+          src="https://www.youtube.com/embed/XX0bCFLb48k?si=aLM77AHSIhFbXJgU"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         ></iframe>{" "}

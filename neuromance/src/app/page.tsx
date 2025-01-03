7c05b02345094c2fd970bfab250c706f25491741
Khyl Nad <khylanad@gmail.com>
2025-01-03 16:56:24 -0700

    cleaned code

--- neuromance/src/app/page.tsx
+++ neuromance/src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import dynamic from "next/dynamic";
 import styles from "./base.module.css";
-import NavBar from "./components/NavBar/NavBar";
 
 // Dynamically import your component to disable SSR
 const AppRouter = dynamic(() => import("./utils/AppRouter/AppRouter"), {

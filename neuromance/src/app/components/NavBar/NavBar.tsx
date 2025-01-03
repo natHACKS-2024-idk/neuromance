b35660d4781438abea620cfc26cdf9305553a41c
Khyl Nad <khylanad@gmail.com>
2025-01-02 19:03:09 -0700

    cleaned navbar

--- neuromance/src/app/components/NavBar/NavBar.tsx
+++ neuromance/src/app/components/NavBar/NavBar.tsx
@@ -10,14 +10,14 @@ export default function Navbar(): JSX.Element {
     <nav className={styles.navbar}>
       <div className={styles.container}>
         <div className={styles.inner}>
-          {/* Logo */}
-          <div className={styles.logoWrapper}>
-            <Brain className={styles.logoBrain} />
-            <Heart className={styles.logoHeart} />
-          </div>
-          {/* Desktop Navigation */}
-          <div className={styles.desktopNav}>
+          <div className={styles.logoNav}>
+            <div className={styles.logoWrapper}>
+              <Brain className={styles.logoBrain} />
+              <Heart className={styles.logoHeart} />
+            </div>
             <NavLinks />
+          </div>
+          <div className={styles.signInUpContainer}>
             <div className={styles.buttons}>
               <button className={styles.signInButton}>Sign In</button>
               <button className={styles.signUpButton}>Sign Up</button>
